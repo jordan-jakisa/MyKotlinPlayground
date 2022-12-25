aeaca4c1ad7121cbdcccdc72f20888907472c430
Jordan <jordan.jakisa@gmail.com>
2022-12-26 00:17:59 +0300

    GroupBy-update

--- src/main/kotlin/GroupBy.kt
+++ src/main/kotlin/GroupBy.kt
@@ -2,11 +2,14 @@ data class Cities(val name: String, val location: String, val address: Int)
 
 val listOfCities = listOf(
     Cities("Kampala", "Uganda", 1200),
-    Cities("Nebbi", "Uganda", 123002),
+    Cities("Arua", "Uganda", 123002),
     Cities("Nairobi", "Kenya", 23000),
-    Cities("Dar es sallam", "Tanzania", 30023)
+    Cities("Dar Es Sallam", "Tanzania", 30023)
 )
 fun main(){
     val list = listOfCities.groupBy { it.location }
-    println("$list")
+    val ugandan = list["Uganda"]
+    if (ugandan != null) {
+        for (i in ugandan) println(i.name)
+    }
 }

2242b9727140c46d0f769d7bbbb4233d7b741dde
Jordan <jordan.jakisa@gmail.com>
2023-01-05 23:18:20 +0300

    When

--- src/main/kotlin/Coroutines.kt
+++ src/main/kotlin/Coroutines.kt
@@ -1,18 +1,28 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import java.util.Timer
+import java.util.TimerTask
+import javax.management.timer.TimerMBean
 
 suspend fun printNumbers(){
     println("Number: $$$")
 }
 
-fun main(){
+class Task: TimerTask() {
+    override fun run() {
+        println("Task is running")
+    }
+}
+
+suspend fun main(){
     /** adding suspend keyword to a function and optionally adding a withContext function in network request
      * Coroutines solve the problems of synchronous programming like ui blocking by long background
      * thread operations. **/
-    runBlocking{
-        withContext(GlobalScope.coroutineContext){
-            printNumbers()
+
+    coroutineScope {
+        launch {
+            /** Runs a statement of code only once **/
+            Timer().scheduleAtFixedRate(Task(), 0, 1000)
+            println("End")
         }
     }
 }
